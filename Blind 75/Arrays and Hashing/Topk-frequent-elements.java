4bf388bb3e97bdb1eca5b71164049aee2cd3b4d9
S.S.Akhilesh Appala <akiakhilesh16@gmail.com>
2022-11-24 10:06:32 +0530

    Update Topk-frequent-elements.java

--- Blind 75/Arrays and Hashing/Topk-frequent-elements.java
+++ Blind 75/Arrays and Hashing/Topk-frequent-elements.java
@@ -10,56 +10,7 @@
 // failing for negative numbers - as sorting a map is failing 
 // will come up with head solution
 
-import java.util.*;
-import java.util.Collection;
-class Solution {
-    public int[] topKFrequent(int[] nums, int k) {
-        //base case
-        if(k == nums.length){
-            return nums;
-        }
-        
-        HashMap<Integer, Integer> map = new HashMap<>();
-        ArrayList<Integer> arrayList = new ArrayList<Integer>(); 
-        for(int i =0;i<nums.length;i++){
-           if(!map.containsKey(nums[i])){
-               map.put(nums[i], 1);
-           }
-            else{
-              int value =  map.get(nums[i]);
-                value ++;
-                map.put(nums[i],value);
-            }
-        }
-        HashMap<Integer, Integer> SortedMap = new HashMap<>();
-        map.entrySet().stream()
-            .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())) 
-            .forEachOrdered(x -> SortedMap.put(x.getKey(), x.getValue()));
-        int count=1;
-         System.out.println(SortedMap);
-        for (Map.Entry<Integer, Integer> en :
-             SortedMap.entrySet()) {
-            if(count>k){
-                break;
-            }
-            else{
-                arrayList.add(en.getKey());
-                count++;
-            }
-            
-        }
-       //  Integer[] arr = new Integer[arrayList.size()];
-       // // int arr = arrayList.toArray();
-       //   arr = arrayList.toArray(arr); 
-       //  int newarr = arr;
-        int[] ret = new int[arrayList.size()];
-    for (int i=0; i < ret.length; i++)
-    {
-        ret[i] = arrayList.get(i).intValue();
-    }
-    return ret;
-        
-        // approach 2 
+
         import java.util.*;
 import java.util.Collection;
 class Solution {
