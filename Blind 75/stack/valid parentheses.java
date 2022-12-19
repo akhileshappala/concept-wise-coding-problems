ac325e3663f1b73942e3416578074242e7a45d6c
S.S.Akhilesh Appala <akiakhilesh16@gmail.com>
2022-12-19 23:11:18 +0530

    Create valid parentheses.java

---
+++ Blind 75/stack/valid parentheses.java
@@ -0,0 +1,37 @@
+
+link : https://leetcode.com/problems/valid-parentheses/description/
+
+class Solution {
+    public boolean isValid(String s) {
+        //base case 
+        if(s.length() %2!=0){
+            return false;}
+        Stack<Character> stack = new Stack<>();
+        for(int i =0;i<s.length();i++){
+            if(stack.isEmpty() && 
+            ( s.charAt(i) == '}' || s.charAt(i) == ']' || s.charAt(i) == ')') ){
+                return false;
+            }
+            else{
+                if(!stack.isEmpty()){
+                    if(stack.peek()=='{' && s.charAt(i)=='}'){
+                        stack.pop(); 
+                    }
+                    else if(stack.peek()=='[' && s.charAt(i)==']'){
+                        stack.pop(); 
+                    }
+                    else if(stack.peek()=='(' && s.charAt(i)==')'){
+                        stack.pop(); 
+                    }
+                    else{
+                        stack.add(s.charAt(i));
+                    }
+                }
+                else{
+                    stack.add(s.charAt(i));
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
