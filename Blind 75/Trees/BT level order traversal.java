1d19ce95eff2b68ed6624ad16f261f85d9164d26
S.S.Akhilesh Appala <akiakhilesh16@gmail.com>
2022-12-24 00:54:01 +0530

    Create BT level order traversal.java

---
+++ Blind 75/Trees/BT level order traversal.java
@@ -0,0 +1,45 @@
+Link : https://leetcode.com/problems/binary-tree-level-order-traversal/description/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+
+        if(root == null ) return result;
+        q.add(root);
+          while(!q.isEmpty()) {
+            int qlen = q.size();
+            List<Integer> level = new ArrayList<>();
+            for(int i =0;i<qlen;i++){
+                TreeNode curr = q.poll();
+                level.add(curr.val);
+                if(curr.left != null){
+                    q.add(curr.left);
+                }
+                if(curr.right != null){
+                    q.add(curr.right);
+                }
+            }
+            result.add(level);
+          }
+            return result;
+
+    }
+
+
+}
