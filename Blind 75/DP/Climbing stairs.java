90956e4d1ecc0d9110aad69e95287b00e64ffcf0
S.S.Akhilesh Appala <akiakhilesh16@gmail.com>
2023-01-01 00:03:15 +0530

    Update Climbing stairs.java

--- Blind 75/DP/Climbing stairs.java
+++ Blind 75/DP/Climbing stairs.java
@@ -1,6 +1,8 @@
 class Solution {
     public int climbStairs(int n) {
+        
         //using fibonacci logic
+        // fibonacci solution starts here
         if(n == 1 ){
             return 1;
         }
@@ -12,5 +14,22 @@ class Solution {
             second = third;
         }
         return second;
+         // fibonacci solution ends here
+        
+        
+        // Using DP 
+        // dp solution starts here
+        int[] dp = new int[n+1];
+        if (n == 1){
+            return 1;
+        }
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i =3 ; i <= n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+         }
+         return dp[n];
+        
+        // dp solution ends here
     }
 }
